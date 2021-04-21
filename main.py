df95ea275afc391e54cd60a9083687a90c91195b
Prathamesh Deshpande <prtdesh25@gmail.com>
2021-04-21 14:00:14 +0530

    more methods added

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 import pandas as pd
-from modules import load_description
+from modules import load_description,clean_description,create_vocab
 filename = "captions.txt"
 file = open(filename,"r")
 doc = file.read()
-
 descriptions = load_description(doc)
+clean_description(descriptions)
+vocab = create_vocab(descriptions)
 print(descriptions["3421129418_088af794f7"])
+print('preprocessed words %d ' % len(vocab))
+print(vocab[:25])
+
+train = []
+num = 0
+for k in descriptions.keys():
+    train.append(k)
+    num += 1
+    if(num>5):
+        break
+
+train_description = dict()
+for img_id in train:
+    train_description[img_id] = list()
+    image_desc = descriptions[img_id]
+    for caption in image_desc:
+        c = 'startseq ' + ''.join(caption) + ' endseq'
+        train_description[img_id].append(c)
+
+print(len(train))
+print(len(train_description))
+
+
+
+
+
--- modules.py
+++ modules.py
@@ -1,13 +1,49 @@
+import string
+
+
+# function to convert text into a dictionary of image_id and image_description
 def load_description(text):
     mapping = dict()
     for line in text.split("\n"):
         token = line.split(",")
-        if len(line) < 2:   # remove short descriptions
+        if len(line) < 2:  # remove short descriptions
             continue
-        img_id = token[0].split('.')[0] # name of the image
-        img_des = token[1]              # description of the image
+        img_id = token[0].split('.')[0]  # name of the image
+        img_des = token[1]  # description of the image
         if img_id not in mapping:
             mapping[img_id] = list()
         mapping[img_id].append(img_des)
     return mapping
 
+
+# function of cleaning image description
+def clean_description(desc):
+    for key, des_list in desc.items():
+        for i in range(len(des_list)):
+            caption = des_list[i]
+            caption = [ch for ch in caption if ch not in string
+                .punctuation]
+            caption = ''.join(caption)
+            caption = caption.split(' ')
+            caption = [word.lower() for word in caption if len(word) > 1 and word.isalpha()]
+            caption = ' '.join(caption)
+            des_list[i] = caption
+
+
+# function to create vocabulary
+def create_vocab(des):
+    all_train_caption = []
+    for k, v in des.items():
+        for cap in v:
+            all_train_caption.append(cap)
+    # considering only words that occur more frequently
+    word_count_threshold = 10
+    word_count = {}
+    n = 0
+    for s in all_train_caption:
+        n += 1
+        for w in s.split():
+            word_count[w] = word_count.get(w, 0) + 1
+
+    vocab = [ws for ws in word_count if word_count[ws] >= word_count_threshold]
+    return vocab
